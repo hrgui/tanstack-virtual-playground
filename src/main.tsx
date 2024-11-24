8c5c40bec488ed19250bc2dadddc0c43c602d5bf
Harman Goei <5001432+hrgui@users.noreply.github.com>
2024-11-23 18:56:42 -0800

    feat: add scroll-polyfill to satisfy chromium 47

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,21 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom/client'
+import "scroll-polyfill/auto";
 
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 
-import { useWindowVirtualizer } from '@tanstack/react-virtual'
+import "./index.css";
+
+import { useWindowVirtualizer } from "@tanstack/react-virtual";
 
 function Example() {
-  const listRef = React.useRef<HTMLDivElement | null>(null)
+  const listRef = React.useRef<HTMLDivElement | null>(null);
 
   const virtualizer = useWindowVirtualizer({
     count: 10000,
     estimateSize: () => 35,
     overscan: 5,
     scrollMargin: listRef.current?.offsetTop ?? 0,
-  })
+  });
 
   return (
     <>
@@ -21,23 +23,21 @@ function Example() {
         <div
           style={{
             height: `${virtualizer.getTotalSize()}px`,
-            width: '100%',
-            position: 'relative',
+            width: "100%",
+            position: "relative",
           }}
         >
           {virtualizer.getVirtualItems().map((item) => (
             <div
               key={item.key}
-              className={item.index % 2 ? 'ListItemOdd' : 'ListItemEven'}
+              className={item.index % 2 ? "ListItemOdd" : "ListItemEven"}
               style={{
-                position: 'absolute',
+                position: "absolute",
                 top: 0,
                 left: 0,
-                width: '100%',
+                width: "100%",
                 height: `${item.size}px`,
-                transform: `translateY(${
-                  item.start - virtualizer.options.scrollMargin
-                }px)`,
+                transform: `translateY(${item.start - virtualizer.options.scrollMargin}px)`,
               }}
             >
               Row {item.index}
@@ -46,17 +46,17 @@ function Example() {
         </div>
       </div>
     </>
-  )
+  );
 }
 
 function App() {
   return (
     <div>
       <p>
-        In many cases, when implementing a virtualizer with a window as the
-        scrolling element, developers often find the need to specify a
-        "scrollMargin." The scroll margin is a crucial setting that defines the
-        space or gap between the start of the page and the edges of the list.
+        In many cases, when implementing a virtualizer with a window as the scrolling element,
+        developers often find the need to specify a "scrollMargin." The scroll margin is a crucial
+        setting that defines the space or gap between the start of the page and the edges of the
+        list.
       </p>
       <br />
       <br />
@@ -64,19 +64,18 @@ function App() {
       <Example />
       <br />
       <br />
-      {process.env.NODE_ENV === 'development' ? (
+      {process.env.NODE_ENV === "development" ? (
         <p>
-          <strong>Notice:</strong> You are currently running React in
-          development mode. Rendering performance will be slightly degraded
-          until this application is build for production.
+          <strong>Notice:</strong> You are currently running React in development mode. Rendering
+          performance will be slightly degraded until this application is build for production.
         </p>
       ) : null}
     </div>
-  )
+  );
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
